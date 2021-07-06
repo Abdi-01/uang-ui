c773a7aa213a7c4e9a0f2c5e0f0d49e216bddf08
idoyudha <idowidya.yudhatama@gmail.com>
2021-07-06 14:12:52 +0700

    update dockerfile and bug in report page

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
-export const URL_API = "http://localhost:4000/"
+// export const URL_API = "http://localhost:4000/"
 
-// export const URL_API = "http://purwadhikafs3.com:8023/"
+export const URL_API = "http://purwadhikafs3.com:8023/"
